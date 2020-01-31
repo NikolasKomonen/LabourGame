03ed03e845edfd45f1962010af04ddf3ceb5db4b
NikolasKomonen <nikolaskomonen@gmail.com>
2020-01-31 05:06:47 -0500

    Working calculations for multipliers
    Signed-off-by: NikolasKomonen <nikolaskomonen@gmail.com>

--- server/calculations.js
+++ server/calculations.js
@@ -1,7 +1,7 @@
 const SQL = require('./database/sqliteIndex')
 const path = require('path')
 
-let db = new SQL(path.join(__dirname, 'database/fukWitMe.sqlite'));
+let db = new SQL(path.join(__dirname, 'database/dbFile.sqlite'));
 db.startDB()
 
 
@@ -92,7 +92,6 @@ class Calculations {
                 calculations.push(this.calculateBaseline(week, campaigns_id))
             }
 
-
             Promise.all(calculations).then((data) => {
                 const hourlyRate = data[0].wage
                 const baseline = this.baseline
@@ -154,125 +153,308 @@ class Calculations {
                     company_wage_history.weeks_week=?
                 ) AS wage_hist
             ON 
-                campaign_user_hours.companies_id=wage_hist.companies_id`, [campaigns_id, lastWeek, campaigns_id, week])
+                campaign_user_hours.companies_id=wage_hist.companies_id;`, [campaigns_id, lastWeek, campaigns_id, week])
     }
 
     updateStrikeTable(week, campaigns_id) {
-        
-        return db.run(`
-            REPLACE INTO user_strike_weeks
-                SELECT
-                    companies_id,
-                    ?,
-                    ?,
-                    sum(selec.strike),
-                    count(*)
+        return db.run("BEGIN TRANSACTION;")
+                .then(() => {
+                    return db.run(`
+                        REPLACE INTO user_strike_weeks
+                            SELECT
+                                companies_id,
+                                ?,
+                                ?,
+                                sum(selec.strike),
+                                count(*)
+                            FROM 
+                                (
+                                SELECT 
+                                    companies_id, strike 
+                                FROM 
+                                    user_game_selections 
+                                WHERE 
+                                    accounts_username 
+                                    IN 
+                                    (SELECT username FROM accounts WHERE campaigns_id=?)
+                                ) AS selec
+                            GROUP BY
+                                selec.companies_id;       
+                    `, [campaigns_id, week, campaigns_id])
+                })
+                .then(() => {
+                    return db.run("COMMIT;")
+                })
+    }
+
+    updateTotalHours(campaigns_id, weeks_week) {
+        return db.all(`
+            REPLACE INTO user_total_company_hours 
+                SELECT 
+                    selec.accounts_username, selec.companies_id, COALESCE(total_hours, 0) + selec.hours
                 FROM 
-                    (
-                    SELECT 
-                        companies_id, strike 
+                    (SELECT 
+                        accounts_username, companies_id, hours 
                     FROM 
                         user_game_selections 
                     WHERE 
-                        accounts_username 
-                        IN 
-                        (SELECT username FROM accounts WHERE campaigns_id=?)
-                    ) AS selec
-                GROUP BY
-                    selec.companies_id;
+                        weeks_week=? 
+                        AND 
+                        accounts_username IN (SELECT username FROM accounts WHERE campaigns_id=?)) AS selec 
+                LEFT JOIN
+                    user_total_company_hours AS totals 
+                ON 
+                    selec.accounts_username=totals.accounts_username
+                    AND
+                    selec.companies_id=totals.companies_id
+            ;
+        `, [weeks_week, campaigns_id])
+    }
+
+    /**
+     * This assumes that user_total_company_hours AND user_career_history
+     * have been brought up to date through all previous weeks
+     */
+    updateCareersTable(campaigns_id, week) {
+        const lastWeek = week - 1
+        const supervisorCandidates = {} // {company_id: []}
+        const noCareerHistory = {}
+        const statementsToRun = []
+        return db.all(`
+            SELECT
+                tohist.accounts_username AS accounts_username, tohist.companies_id AS companies_id, is_supervisor, reached_supervisor, total_hours, regular_hours, supervisor_hours, selec.hours AS last_week_hours
+            FROM
+                (
+                    SELECT 
+                        totals.accounts_username AS accounts_username, totals.companies_id AS companies_id, total_hours, is_supervisor, reached_supervisor 
+                    FROM 
+                            (SELECT * FROM user_total_company_hours WHERE user_total_company_hours.accounts_username IN (SELECT username FROM accounts WHERE campaigns_id=?)) AS totals 
+                        LEFT JOIN 
+                            (SELECT *, max(weeks_week) FROM user_career_history GROUP BY accounts_username, companies_id) AS hist
+                        ON 
+                            totals.accounts_username=hist.accounts_username 
+                            AND
+                            totals.companies_id=hist.companies_id
+                ) AS 
+                    tohist
+                JOIN 
+                    companies
+                ON
+                    tohist.companies_id=companies.id
+                LEFT JOIN 
+                    (SELECT * FROM user_game_selections WHERE weeks_week=?) AS selec
+                ON
+                    tohist.accounts_username=selec.accounts_username
+                    AND
+                    tohist.companies_id=selec.companies_id
                     
-            `, [campaigns_id, week, campaigns_id])
-            
-            
+                
+                
+                `, [campaigns_id, lastWeek])
+        .then((rows) => {
+            rows.forEach((row) => {
+                let c = supervisorCandidates[row.companies_id]
+                if(c == null) {
+                    c = {}
+                    c.candidates = []
+                    c.isSupervisorChosen = false
+                }
+                if(c.isSupervisorChosen) {
+                    return;
+                }
+
+                
+                
+                const totalHours = row.total_hours
+                if(totalHours < row.regular_hours) { // is Nothing
+                    return;
+                }
+
+                const noCareerKey = row.accounts_username + row.companies_id
+                // No career history exists yet for this username+company 
+                if(row.is_supervisor === null) { // null because of LEFT JOIN in query above
+                    noCareerHistory[noCareerKey] = row
+                }
+                
+                if(totalHours >= row.supervisor_hours) { // is eligible for Supervisor
+                    if(row.is_supervisor) {
+                        if(!row.last_week_hours) { // did not work last week, lose their supervisor position
+                            statementsToRun.push(db.run("REPLACE INTO user_career_history VALUES (?, ?, ?, ?, ?)", [row.accounts_username, row.companies_id, week, false, true]))
+                            return
+                        }
+                        else { // keep their supervisor position, making everything else meaningless
+                            c.isSupervisorChosen = true
+                            c.candidates = null // clear out candidates from memory
+                            return
+                        }
+                    }
+                    if(row.reached_supervisor) { // not a supervisor but reached it previously
+                        return;
+                    }
+                    c.candidates.push(row)
+                }
+                else { //is eligible for Regular
+                    delete noCareerHistory[noCareerKey]
+                    statementsToRun.push(db.run("REPLACE into user_career_history VALUES (?, ?, ?, ?, ?)",[row.accounts_username, row.companies_id, week, false, false]))
+                }
+            })
+
+            Object.keys(supervisorCandidates).forEach((key) => {
+                const candidates = supervisorCandidates[key].candidates
+                const length = candidates.length
+                const newSupervisorIndex = Math.floor(Math.random() * Math.floor(length - 1))
+                const newSupervisor = candidates[newSupervisorIndex]
+                const noCareerKey = newSupervisor.accounts_username + newSupervisor.companies_id
+                delete noCareerHistory[noCareerKey]
+                statementsToRun.push(db.run("REPLACE INTO user_career_history VALUES (?, ?, ?, ?, ?)",[newSupervisor.accounts_username, newSupervisor.companies_id, week, true, true]))
+            })
+
+            return statementsToRun
+        })
+        .then((statementsToRun) => {
+            return db.run("BEGIN TRANSACTION;")
+            .then(() => {
+                return Promise.all(statementsToRun)
+            })
+            .then(() => {
+                return db.run("COMMIT;")
+            })
+        })
+        
         
     }
 
-    updateCareersTable() {
-        db.run(`
-            INSERT INTO user_career_history VALUES(
-                SELECT * 
-        ) ON CONFLICT DO UPDATE SET `)
+    /**
+     * Main function.
+     * 
+     * Think of this function as a verification function, where it will
+     * go from the beginning of the game and process the game data.
+     * 
+     * For each week the hourly rate will be calculated with the data
+     * from the previous week. The end result will be hourly rates up
+     * to and including for the given week.
+     * 
+     * Preferably, for each week the individual week would be calculated
+     * relying that the week's before it were previously calculated.
+     * This can be done with calculateOnlyWeek().
+     * @param {*} campaigns_id 
+     * @param {*} week 
+     */
+    calculateUpToAndIncludingWeek(campaigns_id, week) {
+        return this.c(campaigns_id, 2, week)
     }
 
-    updateTotalHours(campaigns_id) {
-        return db.run(`
-            REPLACE INTO user_total_company_hours VALUES (
-                SELECT 
-                    accounts_username,
-                    companies_id,
-                    sum(hours)
-                FROM
-                    (SELECT * FROM user_game_selections WHERE accounts_username IN (SELECT * FROM accounts WHERE campaigns_id=?)) AS accs
-                GROUP BY
-                    accs.accounts_username,
-                    accs.companies_id
-                    
-            )
-        
-        `)
+    c(campaigns_id, current_week, max_week) {
+        if(current_week > max_week) {
+            return
+        }
+        else {
+            return this.calculateOnlyWeek(campaigns_id, current_week).then(() => {
+                return this.c(campaigns_id, current_week + 1, max_week)
+            })
+        }
     }
+
+    calculateOnlyWeek(campaigns_id, week) {
+        // ** DO NOT MODIFY ANY OF THE FOLLOWING **
+
+        // 1) Calculate the Hourly Rate for the given week.
+        //    In this case Week=1 is already done (all 0).
+        //    IMPORTANT: Do for each campaign.
+        
+        const lastWeek = week - 1;
+
+        return this.getWagesForThisWeek(lastWeek, campaigns_id) // JSON's of this week's wages
+        .then((result) => {
+            // Inserting the wages into the DB
+            return db.run("BEGIN TRANSACTION;").then(() => {
+                const updates = result.map(c => {
+                    return db.run("REPLACE INTO company_wage_history VALUES (?, ?, ?, ?)", [c.companies_id, campaigns_id, week, c.hourlyRate])
+                });
+                return Promise.all(updates).then(() => {
+                    return db.run("COMMIT;")
+                })
+            })
+        })
+        .then(() => { // Updated the DB user_strike_weeks table
+            return Promise.all([
+                this.updateStrikeTable(lastWeek, campaigns_id),
+                this.updateTotalHours(campaigns_id, lastWeek)
+                .then(() => {
+                    this.updateCareersTable(campaigns_id, week)
+                })
+            ])
+            
+        })
+        .then(() => {
+            // // 2) Get Modifier Rates (This includes updating them first)
+            return Promise.all([
+            // // 2a) Fixed Events
+            db.all("SELECT companies_id, description, event_types_id, event_data FROM fixed_event_cards WHERE weeks_week=?;", [week]),
+            // // 2b) Event Cards
+
+            // ******* NEED TO CHECK IF THIS ALREADY EXISTS IN THE EVENT CARD HISTORY
+            db.get("SELECT id, companies_id, description, event_types_id, event_data FROM event_card_history JOIN event_cards ON event_card_history.event_cards_id=event_cards.id AND event_card_history.weeks_week=?", [lastWeek])
+            .then((existingCard) => {
+                if(existingCard) {
+                    return existingCard
+                }
+                return db.get("SELECT id, companies_id, description, event_types_id, event_data FROM event_cards ORDER BY RANDOM() LIMIT 1;")
+            }),
+            // // 2c) Company Strike
+            db.all("SELECT companies_id, workers_striked, total_workers FROM user_strike_weeks WHERE campaigns_id=? and weeks_week=?", [campaigns_id, lastWeek])
+            // // 2d) Career Boost
+            
+
+            ])
+        })
+        .then((data) => {
+            // update histories
+            const eventCard = data[1]
+            return Promise.all([
+                db.run(`INSERT OR IGNORE INTO event_card_history VALUES (?, ?)`, [lastWeek, eventCard.id]) // This not replace because if it is randomly selected and may have already been chosen
+            ]).then(() => {
+                return data
+            })
+        })
+        .then((data) => {
+            // 3) Calculate each individual user's score
+            return this.calculateUserScores(week, campaigns_id, data[0], data[1], data[2], null).then((res) => {
+                const all = {}
+                res.forEach(row => {
+                    
+                    if(all[row.username] == null) {
+                        all[row.username] = 0
+                    }
+                    all[row.username]+=Math.round((row.wage*row.hours) * 100) / 100
+                })
+                return all
+            })
+        })
+        .then((all) => {
+            return db.all("SELECT username FROM accounts WHERE campaigns_id=?", [campaigns_id]).then((users) => {
+                const final = []
+                
+                users.forEach(user => {
+                    const username = user.username
+                    let profit = all[username]
+                    if(!profit) {
+                        profit = 0
+                    }
+                    final.push({user: username, profit: profit})
+                })
+                
+                
+                console.log(final)
+            })
+            
+        })
+    }
+
+    
 }
 
+const c = new Calculations(db);
+const campaigns_id = 2
 
-// *** START HERE ***
-// Modify these values
-const campaigns_id = 1;
-const week = 2; 
-
-// ** DO NOT MODIFY ANY OF THE FOLLOWING **
-
-// 1) Calculate the Hourly Rate for the given week.
-//    In this case Week=1 is already done (all 0).
-//    IMPORTANT: Do for each campaign.
-const c = new Calculations(db)
-const lastWeek = week - 1;
-
-c.getWagesForThisWeek(lastWeek, campaigns_id) // JSON's of this week's wages
-.then((result) => {
-    // Inserting the wages into the DB
-    return db.run("BEGIN TRANSACTION;").then(() => {
-        const updates = result.map(c => {
-            return db.run("REPLACE INTO company_wage_history VALUES (?, ?, ?, ?)", [c.companies_id, campaigns_id, week, c.hourlyRate])
-        });
-        return Promise.all(updates).then(() => {
-            return db.run("COMMIT;")
-        })
-    })
-})
-.then(() => { // Updated the DB user_strike_weeks table
-    return Promise.all([
-        c.updateStrikeTable(lastWeek, campaigns_id)
-        // c.updateTotalHours(campaigns_id)
-        // .then(() => {
-        //     c.updateCareersTable(campaigns_id, week)
-        // })
-    ])
-     
-})
-.then(() => {
-    // // 2) Get Modifier Rates (This includes updating them first)
-    return Promise.all([
-    // // 2a) Fixed Events
-    // const fixedEvents={id: {description: "", type: 1||2, data: 0.2||-0.2, company_id: 55}}
-    db.all("SELECT companies_id, description, event_types_id, event_data FROM fixed_event_cards WHERE weeks_week=?;", [week]),
-    // // 2b) Event Cards
-    // const weeklyEvents={id: {description: "", type: 1||2, data: 0.2||-0.2, company_id: 55}}
-    db.get("SELECT companies_id, description, event_types_id, event_data FROM event_cards ORDER BY RANDOM() LIMIT 1;"),
-    // // 2c) Company Strike
-    // const strikeEvents={company_id: {workers_striked: 55, total_workers: 55}}
-    db.all("SELECT companies_id, workers_striked, total_workers FROM user_strike_weeks WHERE campaigns_id=? and weeks_week=?", [campaigns_id, lastWeek])
-    
-    // // 2d) Career Boost
-    // const careerBoosts={username: {company_id: isSupervisor}}
-
-    ])
-})
-.then((data) => {
-    // 3) Calculate each individual user's score
-    c.calculateUserScores(week, campaigns_id, data[0], data[1], data[2], null).then((res) => {
-        console.log(res)
-    })
-})
-
-
-
+c.calculateUpToAndIncludingWeek(campaigns_id, 2)
--- server/database/sqliteIndex.js
+++ server/database/sqliteIndex.js
@@ -4,6 +4,8 @@ class SQL {
     constructor(dbFilePath) {
         this.dbFilePath = dbFilePath
     }
+
+
     
     startDB() {
         return new Promise((resolve, reject) => {
@@ -24,6 +26,20 @@ class SQL {
     run(sql, params = []) {
         return new Promise((resolve, reject) => {
             this.db.run(sql, params, function (err) {
+                if (err) {
+                    console.log('Error running sql ' + sql)
+                    console.log(err)
+                    reject(err)
+                } else {
+                    resolve({ id: this.lastID })
+                }
+            })
+        })
+    }
+
+    exec(sql) {
+        return new Promise((resolve, reject) => {
+            this.db.exec(sql, function (err) {
                 if (err) {
                     console.log('Error running sql ' + sql)
                     console.log(err)
